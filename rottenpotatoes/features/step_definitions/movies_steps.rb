39ac986171385a8c5416a9f8468b5aaf6b1178d2
EC2 Default User <ec2-user@ip-172-31-15-54.us-east-2.compute.internal>
2019-02-26 20:12:09 +0000

    Scenario 1 completed

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -23,3 +23,8 @@ Then /I should see all the movies/ do
     step %{I should see "#{movie.title}"}
   end
 end
+
+Then /^the director of "(.*?)" should be "(.*?)"$/ do |movie_title, new_director|
+  movie = Movie.find_by_title movie_title
+  movie.director.should == new_director
+end
